c209e6d50607bfc56f1ee531b6d37499f7fce02a
Grase Thessalaonika <gracethessa1@gmail.com>
2025-05-24 16:55:09 +0700

    Tambahkan CopyWebpackPlugin untuk deployment

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
